76da5d99f1096d43b09ee69ac3e61220fcc00361
UBUKAWA Taro <59204379+ubukawa@users.noreply.github.com>
2023-11-26 07:58:01 +0900

    rev

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 //test
+const config = require('config')
 const fs = require('fs')
+import { json-text-sequence } from './node_modules/json-text-sequence'
+const ogr2ogrPath = config.get('ogr2ogrPath')
 
+console.log(ogr2ogrPath)
+
+/*
 fs.readFile('src/01223__13_r_2023.geojson','UTF-8',function(err,data){
 
  console.log(data)
 
 })
+*/
