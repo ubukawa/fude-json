c2625e9aab7ba5292e5b7057e05cd5a1f350b60f
UBUKAWA Taro <59204379+ubukawa@users.noreply.github.com>
2023-11-25 23:27:01 +0900

    rev

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fude-json
 test of chizu XML
+
+# ソース
+G空間情報センターのGeoJSON（根室市、松前町の2023年）
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 //test
+const fs = require('fs')
+
+fs.readFile('src/01223__13_r_2023.geojson','UTF-8',function(err,data){
+
+ console.log(data)
+
+})
